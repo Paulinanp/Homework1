5eb62da89a44eb3dd986eab97b2864232e6a3911
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:59:05 +0300

    Add files via upload

---
+++ Ex.10.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int year;
+    cin >> year;
+
+	if (year % 2 == 0)
+	{
+		if (year % 100 != 0)
+		{
+			cout << year << " is a loop year.";
+		}
+		else
+		{
+			cout << year << " is not a loop year.";
+		}
+	}
+	else
+	{
+		cout << year << " is not a loop year.";
+	}
+	return 0;
+}
