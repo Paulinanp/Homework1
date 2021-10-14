68089175960dddaf64cdca0f56dafa3ce0ef8a21
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:57:57 +0300

    Add files via upload

---
+++ Ex.7.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	int sideA, sideB, sideC;
+
+	cin >> sideA >> sideB >> sideC;
+
+	int perimeter = sideA + sideB + sideC;
+
+	int p = (sideA + sideB + sideC) / 2;
+
+	int area = sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+
+	cout << "Perimeter: " << perimeter << " Area: " << area;
+
+	return 0;
+}
