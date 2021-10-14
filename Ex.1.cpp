0af92976533ad22abc5ca56176cf45fd9e4626ef
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:55:28 +0300

    Add files via upload

---
+++ Ex.1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int number, firstNum, secondNum, thirdNum, fourthNum, sum;
+	cin >> number;
+
+	firstNum = number / 1000;
+	cout << "first digit: " << firstNum << endl;
+
+	secondNum = (number / 100) % 10;
+	cout << "second digit: " << secondNum << endl;
+
+	thirdNum = (number / 10) % 10;
+	cout << "third digit: " << thirdNum << endl;
+
+	fourthNum = number % 10;
+	cout << "fourth digit: " << fourthNum << endl;
+
+	sum = firstNum + secondNum + thirdNum + fourthNum;
+	cout << "sum: " << sum;
+
+	return 0;
+}
+
