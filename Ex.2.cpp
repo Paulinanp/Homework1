cddc646e695434eed3e0fd50d291cc8526e9089e
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:56:06 +0300

    Add files via upload

---
+++ Ex.2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number, reminder;
+    int reversedNum = 0;
+    
+    cin >> number;
+
+    while (number != 0)
+    {
+        reminder = number % 10;
+        reversedNum = reversedNum * 10 + reminder;
+        number /= 10;
+    }
+
+    cout << reversedNum;
+
+    return 0;
+}
+
