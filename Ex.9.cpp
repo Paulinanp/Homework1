df50aa52cfdc55595392b185b364d6400686f491
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:58:43 +0300

    Add files via upload

---
+++ Ex.9.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double numX;
+    double a, b;
+
+    cin >> numX >> a >> b;
+
+    if (numX >= a && numX <= b)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+
+    return 0;
+}
