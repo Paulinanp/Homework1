1f05e63ff50b9598e91e334e5e04b7745d260d7d
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:58:22 +0300

    Add files via upload

---
+++ Ex.8.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	double num1, num2, num3;
+
+	cin >> num1 >> num2 >> num3;
+
+	double maxNumber, minNumber;
+
+	if (num1 > num2 && num1 > num3)
+	{
+		maxNumber = num1;
+	}
+	else if (num2 > num1 && num2 > num3)
+	{
+		maxNumber = num2;
+	}
+	else if (num3 > num1 && num3 > num2)
+	{
+		maxNumber = num3;
+	}
+
+	if (num1 < num2 && num1 < num3)
+	{
+		minNumber = num1;
+	}
+	else if (num2 < num1 && num2 < num3)
+	{
+		minNumber = num2;
+	}
+	else if (num3 < num1 && num3 < num2)
+	{
+		minNumber = num3;
+	}
+
+	cout << maxNumber << " " << minNumber;
+
+	return 0;
+}
