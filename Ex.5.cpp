e663f654e6f61a18ad84849df68c1f21b0f76552
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:57:18 +0300

    Add files via upload

---
+++ Ex.5.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+
+    cin >> num1;
+    cin >> num2;
+
+    if (num1 / num2)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+
+    return 0;
+}
+
