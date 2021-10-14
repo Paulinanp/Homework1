908a2a5ab97418ec566bd18be99e43aa4ce4040a
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:56:36 +0300

    Add files via upload

---
+++ Ex.3.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, multiplication, lastNum;
+    cin >> num1 >> num2;
+
+    multiplication = num1 * num2;
+    lastNum = multiplication % 10;
+    bool isDigit = multiplication % 2 == 0;
+
+    cout << multiplication << ", " << lastNum << ", " << isDigit;
+
+    return 0;
+}
+
