3d13c957e04b13e7a140db6f79cbcfe14d61a28d
Paulina Peneva <92331926+Paulinanp@users.noreply.github.com>
2021-10-14 18:57:36 +0300

    Add files via upload

---
+++ Ex.6.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sideA, sideB, sideC;
+
+    cin >> sideA >> sideB >> sideC;
+
+    if (sideA + sideB > sideC && sideA + sideC > sideB && sideB + sideC > sideA)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+
+    return 0;
+}
+
